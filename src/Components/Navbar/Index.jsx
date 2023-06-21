518ef2d36c4a1bf57dec559ec3546c4413c5706e
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-06-21 18:32:24 +0600

    Navbar Area Added

--- src/Components/Navbar/Index.jsx
+++ src/Components/Navbar/Index.jsx
@@ -4,9 +4,60 @@ import './Style.css'
 const Navbar = () => {
     return (
         <React.Fragment>
-            <div className="container">
-                <h3>Navbar Area</h3>
-            </div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                    <a className="navbar-brand" href="##">
+                        <picture>
+                            <img src="images/logo.png" alt="logo" />
+                        </picture>
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i className="fa-solid fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="##">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#about">
+                                    About US
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#portfolio">
+                                    Our Portfolio
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#service">
+                                    Testimonial
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#pricing">
+                                    Pricing Plan
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#footer">
+                                    Contact Us
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </React.Fragment>
     )
 }
