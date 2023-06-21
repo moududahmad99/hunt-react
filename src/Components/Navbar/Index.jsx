6b2805bbd08ead4a8a270b22a8ed6a44e43f5785
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-06-21 18:18:26 +0600

    Setup Environment

---
+++ src/Components/Navbar/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Style.css'
+
+const Navbar = () => {
+    return (
+        <React.Fragment>
+            <div className="container">
+                <h3>Navbar Area</h3>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Navbar;
