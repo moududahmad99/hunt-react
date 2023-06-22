f2f8f2d47922630761ed781a31243ec9164153cc
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-06-22 17:38:14 +0600

    About Area Added

---
+++ src/Components/About/Index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import './Style.css'
+
+const About = () => {
+    return (
+        <section id="about">
+            <div className="container">
+                <div className="row justify-content-center py-5">
+                    <div className="col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div className="heading text-center">
+                            <h3>About Us</h3>
+                            <p>
+                                Lorem Ipsum is simply dummy text often heg and namtypesetting industry. Loremumhaseen the my
+                                naindustry's standardmy text ever the 1500s of the end.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center mb-5">
+                    <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div className="about-text top text-center">
+                            <h3>What We Are?</h3>
+                            <p>
+                                Lorem Ipsum is simply dummy text often heg and namtypesetting industry. Loremumhaseen the my
+                                naindustry's standardmy text ever the 1500s of the end.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row align-items-center">
+                    <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div className="about-text d-none d-lg-block left text-lg-end">
+                            <h3>What We Can?</h3>
+                            <p>
+                                Lorem Ipsum is simply dummy text often heg and namtypesetting industry. Loremumhaseen the my
+                                naindustry's standardmy text ever the 1500s of the end.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div className="about-img"></div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div className="about-text d-none d-lg-block right">
+                            <h3>What We Do?</h3>
+                            <p>
+                                Lorem Ipsum is simply dummy text often heg and namtypesetting industry. Loremumhaseen the my
+                                naindustry's standardmy text ever the 1500s of the end.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center mt-5">
+                    <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div className="about-text bottom text-center">
+                            <h3>Why Choose Us?</h3>
+                            <p>
+                                Lorem Ipsum is simply dummy text often heg and namtypesetting industry. Loremumhaseen the my
+                                naindustry's standardmy text ever the 1500s of the end.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Index'
 import Banner from '../../Components/Banner/Index'
+import About from '../../Components/About/Index'
 
 const Home = () => {
     return (
         <React.Fragment>
             <Navbar />
             <Banner />
+            <About />
         </React.Fragment>
     )
 }
